074222d657fa3ce4db6cd3f3d0b7014c94eadc95
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-16 13:43:33 +0000

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+/* Opis
+
+Napisz funkcję ze zmiennymi lokalnymi, która liczy sumę kwadratów elementów tablicy [0,1,2,3,4,5]. Funkcja przyjmuje jeden parametr - w/w tablicę z liczbami. Funkcja zwraca sumę kwadratow. Wyświetl wynik działania funkcji w konsoli.
+
+Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+
+Podpowiedź przed wysłaniem zadania do sprawdzenia:
+- użyj w zadaniu pętli, którą uważasz za słuszną i klasy Math.
+ */
